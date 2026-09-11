d8aeea10e0d27492591536be79dd3967a6ebd11c
oathkite <kita@impv.co.jp>
2026-09-11 18:00:52 +0900

    style(pilot): 搭乗パイロットの目と口元をHUDに近づける

--- assets/workbench/pilot-generic-v2/build.mjs
+++ assets/workbench/pilot-generic-v2/build.mjs
@@ -6,7 +6,10 @@ const poses = [
 ];
 const eye = (x, expression) => expression === 'closed' ? `<path d="M${x} 10h6v2h-6z" fill="#11170e"/>`
   : expression === 'hit' ? `<path d="M${x} 7h2v2h2v2h2v2h-2v-2h-2V9h-2zM${x+4} 7h2v2h-2v2h-2v2h-2v-2h2V9h2z" fill="#11170e"/>`
-  : `<rect x="${x}" y="5" width="7" height="10" fill="#f5f0ce"/><rect x="${x+3}" y="7" width="4" height="7" fill="#112a16"/><rect x="${x+3}" y="7" width="2" height="2" fill="#fffdf0"/>${expression === 'half' ? `<rect x="${x}" y="5" width="7" height="4" fill="#759735"/>` : ''}`;
+  : `<path d="M${x+2} 3h5v1h2v2h1v7h-1v2h-2v1h-5v-1h-2V5h2z" fill="#11170e"/>
+<path d="M${x+2} 4h5v2h2v7h-2v2h-5v-2H${x+1}V6h1z" fill="#f5f0ce"/>
+<path d="M${x+4} 6h3v2h1v4h-1v2h-3v-1h-1V8h1z" fill="#112a16"/>
+<rect x="${x+4}" y="6" width="2" height="3" fill="#fffdf0"/>${expression === 'half' ? `<path d="M${x+1} 4h7v4h-7z" fill="#759735"/>` : ''}`;
 const frames = poses.map(([dx,dy,expression], i) => `<g transform="translate(${i%4*192+44} ${Math.floor(i/4)*160+61})">
 <path d="M9 22h13v2h4v10h-2v3h6v3H7v-2H4V28h2v-4h3z" fill="#151a0f"/>
 <path d="M9 24h12v3h3v6h-3v4h-6v-3H7v-7h2z" fill="#666738"/><path d="M8 27h4v7h-5v-5h1zM15 25h4v11h-3z" fill="#939054"/>
@@ -16,8 +19,8 @@ const frames = poses.map(([dx,dy,expression], i) => `<g transform="translate(${i
 <path d="M3 9h4V4h4V1h8v3h5v3h5v5h3v6h-3v4h-6v2H10v-2H5v-3H2v-7h1z" fill="#11170e"/>
 <path d="M5 10h4V5h3V3h6v3h5v3h4v5h3v3h-3v3h-5v2H11v-2H7v-3H4v-5h1z" fill="#90bb43"/>
 <path d="M7 10h3V6h3V4h4v3h-4v4h-3v5H7zM20 9h6v5h-3v-2h-3z" fill="#b5d950"/>
-<path d="M7 17h5v2h15v1h-5v2H11v-2H7z" fill="#e4e49c"/>
-${eye(11,expression)}${eye(21,expression)}
-<path d="M17 17h5v1h6v-1h2v2h-8v-1h-5z" fill="#172514"/>
+<path d="M6 17h6v1h16v3h-5v2H11v-2H7v-2H6z" fill="#e4e49c"/>
+${eye(9,expression)}${eye(20,expression)}
+<path d="M10 17h2v2h5v1h7v-1h4v-2h2v3h-5v1h-9v-1h-5v-1h-1z" fill="#172514"/>
 </g></g>`).join('\n');
 writeFileSync(new URL('./pilot-frog.svg', import.meta.url), `<svg xmlns="http://www.w3.org/2000/svg" width="768" height="640" viewBox="0 0 768 640" shape-rendering="crispEdges">${frames}</svg>\n`);
